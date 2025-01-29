7f259bd2fce08ad913aa32a2c21901ba33533580
Narendrakumar S <narendrakumarsuresh57@gmail.com>
2025-01-29 11:53:13 +0530

    done

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+require('dotenv').config();
+
+
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  if (process.env.IS_ADMIN === 'true') {
+    res.send({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+  } else {
+    res.send({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+  }
 });
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
